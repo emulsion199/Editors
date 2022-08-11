68bb7e27ed9fea6ecc94d34f5ec9cc0d36ec0b0d
emulsion199 <wodnjs471@naver.com>
2022-08-11 13:22:12 +0900

    [feat]: move rectangle/line

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -1,40 +1,99 @@
 import {useLayoutEffect,useState} from 'react'
 import rough from 'roughjs/bundled/rough.esm'
 const generator= rough.generator();
-const createElement=(tool,x1,y1,x2,y2)=>
+const createElement=(index,x1,y1,x2,y2,tool)=>
 {
-    const roughElement=tool==0?generator.line(x1,y1,x2,y2):generator.rectangle(x1,y1,x2-x1,y2-y1)
-    return {x1,y1,x2,y2,roughElement}
+    console.log(tool)
+    const roughElement=tool=='line'?generator.line(x1,y1,x2,y2):tool=='rectangle'?generator.rectangle(x1,y1,x2-x1,y2-y1):null
+    return {index,x1,y1,x2,y2,tool,roughElement}
+}
+
+const distance=(a,b)=>Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y,2))
+const isWithinElement=(x,y,element)=>
+{
+    const {tool,x1,x2,y1,y2}=element;
+    if(tool==='rectangle')
+    {
+        const minX=Math.min(x1,x2);
+        const maxX=Math.max(x1,x2);
+        const minY=Math.min(y1,y2);
+        const maxY=Math.max(y1,y2);
+        return x>=minX && x<=maxX && y>=minY && y<=maxY;
+    }else{
+        const a={x:x1,y:y1}
+        const b={x:x2,y:y2}
+        const c={x,y}
+
+        const offset= distance(a,b) - (distance(a,c)+distance(b,c))
+
+        return Math.abs(offset) < 1;
+    }
+}
+const getElementAtPosition=(x,y,elements)=>
+{
+    return elements.find(element=>isWithinElement(x,y,element))
 }
 const Editor=()=>
 {
-    const [tool,setTool]=useState(0)
+    const [tool,setTool]=useState('selection')
     const [elements,setElements]=useState([])
-    const [drawing,setDrawing]=useState(false)
-    const onmousedown=(e)=>
+    const [action,setAction]=useState('none')
+    const [selectedElement,setSelectedElement]=useState(null);
+    const updateElement=(index,x1,y1,x2,y2,tool)=>
     {
-        setDrawing(true);
-        const {clientX,clientY}=e;
-
-        const element=createElement(tool,clientX,clientY,clientX,clientY)
-
-        setElements(prevState=>[...prevState, element])
-    }
-    const onmousemove=(e)=>
-    {
-        if(!drawing) return;
-        const {clientX,clientY}=e;
-        const index=elements.length-1;
-        const {x1,y1}=elements[index];
-        const updatedElement=createElement(tool,x1,y1,clientX,clientY)
+        const updatedElement=createElement(index,x1,y1,x2,y2,tool)
         const elementsCopy=[...elements];
         elementsCopy[index]=updatedElement;
         setElements(elementsCopy)
     }
+    const onmousedown=(e)=>
+    {
+        const {clientX,clientY}=e;
+        if(tool==='selection')
+        {
+            const element=getElementAtPosition(clientX,clientY,elements)
+            console.log(element)
+            if(element)
+            {
+                const offsetX=clientX-element.x1;
+                const offsetY=clientY-element.y1;
+                setAction('moving')
+                setSelectedElement({...element,offsetX,offsetY})
+            }
+        }
+        else{
+            const id = elements.length;
+            setAction('drawing');
+            const element=createElement(id,clientX,clientY,clientX,clientY,tool)
+            setElements(prevState=>[...prevState, element])
+        }
+    }
+    const onmousemove=(e)=>
+    {
+        const {clientX,clientY}=e;
+        if(action==='drawing')
+        { 
+            const index=elements.length-1;
+            const {x1,y1}=elements[index];
+            updateElement(index,x1,y1,clientX,clientY,tool)
+        }
+        if(action==='moving')
+        {
+            const {index,x1,x2,y1,y2,tool,offsetX,offsetY} = selectedElement
+            const width=x2-x1;
+            const height=y2-y1;
+            const newX=clientX-offsetX
+            const newY=clientY-offsetY
+            updateElement(index,newX,newY,newX+width,newY+height,tool);
+        }
+    }
     const onmouseup=(e)=>
     {
-        setDrawing(false);
+        setAction('selection');
+        setSelectedElement(null)
     }
+
+    //캔버스 생성//
     useLayoutEffect(()=>{
         const canvas=document.getElementById('canvas');
         const context=canvas.getContext('2d');
@@ -42,20 +101,24 @@ const Editor=()=>
         const roughCanvas=rough.canvas(canvas)
         elements.forEach(({roughElement})=>roughCanvas.draw(roughElement));
       },[elements])
+
+
+
+
     return(
         <div>
-            
-    <canvas
-    id="canvas"
-    width={window.innerWidth}
-    height={window.innerHeight-30}
-    onMouseDown={onmousedown}
-    onMouseMove={onmousemove}
-    onMouseUp={onmouseup}>
-    </canvas>
-    <button onClick={()=>{setTool(0)}}>선</button>
-            <button onClick={()=>{setTool(1)}}>직사각형</button>
-    </div>
+            <canvas
+            id="canvas"
+            width={window.innerWidth}
+            height={window.innerHeight-30}
+            onMouseDown={onmousedown}
+            onMouseMove={onmousemove}
+            onMouseUp={onmouseup}>
+            </canvas>
+            <button onClick={()=>{setTool('line')}}>선</button>
+            <button onClick={()=>{setTool('rectangle')}}>직사각형</button>
+            <button onClick={()=>{setTool('selection')}}>선택</button>
+        </div>
     )
 }
 export default Editor
